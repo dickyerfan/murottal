063354f932737b3f97ceba4213c6f57fc1c8c593
dickyerfan <dickyerfan@gmail.com>
2021-12-22 15:18:37 +0700

    menambah judul bahasa arab

--- assets/mishaari.js
+++ assets/mishaari.js
@@ -3,8 +3,8 @@ $.getJSON('https://raw.githubusercontent.com/penggguna/QuranJSON/master/quran.js
     $.each(data, function(i, data){
         $('#daftar_menu').append(`
         <div class="col-md-6 col-lg-4 mb-1">
-            <li class="card ps-4 fs-3 shadow">
-            ${data.number_of_surah}. ${data.name}
+            <li class="card ps-4 fs-4 shadow">
+            ${data.number_of_surah}. ${data.name} / ${data.name_translations.ar}
                 <audio controls>
                     <source src="${data.recitation}" type="audio/mpeg">
                 </audio>
